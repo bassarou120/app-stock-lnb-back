95966963fb014e9d0c4e26d7228d9b16f9bc6b7f
DJAFF00 <djaffmanakel259451@gmail.com>
2025-11-03 15:34:23 +0100

    commit

--- resources/views/pdf/rapport-periodique.blade.php
+++ resources/views/pdf/rapport-periodique.blade.php
@@ -171,6 +171,7 @@
                             <td>{{ $donnees['entrees'] }}</td>
                             <td>{{ $donnees['retours'] }}</td>
                             <td>{{ $donnees['entrees'] + $donnees['retours'] }}</td>
+                            <!-- <td>{{ $donnees['entrees'] + $donnees['retours'] }}</td> -->
                             <td>{{ $donnees['sorties_par_categorie']['Dotation Agences'] ?? 0 }}</td>
                             <td>{{ $donnees['sorties_par_categorie']['Dotation Chef Garage'] ?? 0 }}</td>
                             <td>{{ $donnees['sorties_par_categorie']['Groupe Electrogène'] ?? 0 }}</td>
