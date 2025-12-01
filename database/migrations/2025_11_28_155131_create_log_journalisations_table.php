7ced9a584ca3bb4c89fae842cd94ba86a293393b
Deveventlab <rolanddevjunior@gmail.com>
2025-12-01 08:47:29 +0100

    update du lundi

--- database/migrations/2025_11_28_155131_create_log_journalisations_table.php
+++ database/migrations/2025_11_28_155131_create_log_journalisations_table.php
@@ -19,9 +19,10 @@ return new class extends Migration
             $table->string('user_agent')->nullable();
 
             // ✅ Clé étrangère UUID vers users
-            $table->foreignId('user_id')
-                ->nullable() // Permettre la valeur NULL (pour la connexion échouée et on delete set null)
-                ->constrained('users') // Assurez-vous que c'est bien le nom de votre table utilisateurs
+            $table->uuid('user_id')->nullable();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
                 ->onUpdate('cascade')
                 ->onDelete('set null');
 
