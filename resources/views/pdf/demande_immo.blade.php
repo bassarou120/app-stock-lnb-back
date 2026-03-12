0e6e4fecd65aa7c09ebb6d2b460a0506aa95d57b
philemon_dev <etoundelight1@gmail.com>
2026-03-12 09:01:15 +0100

    doc

--- resources/views/pdf/demande_immo.blade.php
+++ resources/views/pdf/demande_immo.blade.php
@@ -6,25 +6,47 @@
 
 <style>
 
+@page{
+    size: A4 landscape;
+    margin: 10mm;
+}
+
 body{
-font-family: Arial;
-font-size:12px;
+    font-family: Arial, sans-serif;
+    font-size:11pt;
 }
 
 table{
-width:100%;
-border-collapse:collapse;
+    width:100%;
+    border-collapse:collapse;
 }
 
 th,td{
-border:1px solid black;
-padding:6px;
-text-align:left;
+    border:1px solid black;
+    padding:6px;
+}
+
+th{
+    background:#f2f2f2;
 }
 
 .header{
-text-align:center;
-margin-bottom:20px;
+    text-align:center;
+    margin-bottom:15px;
+}
+
+.header-table td{
+    border:none;
+}
+
+.info-table td{
+    border:none;
+    padding:4px;
+}
+
+.signatures td{
+    height:120px;
+    vertical-align:top;
 }
 
 </style>
@@ -33,7 +55,19 @@ margin-bottom:20px;
 
 <body>
 
-<div class="header">
+<!-- HEADER -->
+<table class="header-table">
+
+<tr>
+
+<td width="30%">
+République du Bénin<br>
+LNB - Lotterie Nationale du Bénin SA
+</td>
+
+<td width="40%" class="header">
+
+<img src="images/logo1.png" style="height:50px;"><br>
 
 <h2>FICHE DEMANDE D'IMMOBILISATION</h2>
 
@@ -41,43 +75,56 @@ N° {{ $numeroFiche }}
 
 <br>
 
-Généré le : {{ date('d/m/Y H:i') }}
+<small>Généré le : {{ date('d/m/Y H:i') }}</small>
 
-</div>
+</td>
 
-<table>
+<td width="30%" style="text-align:right">
+
+{{-- Modèle n°2 --}}
+
+</td>
+
+</tr>
+
+</table>
+
+<br>
+
+<!-- INFORMATIONS -->
+<table class="info-table">
 
 <tr>
+
 <td width="50%">
 
-<strong>Demandeur</strong><br>
+<strong>Informations du Demandeur</strong>
 
-Nom : {{ $demande->employe->nom ?? '-' }}
+<br><br>
 
+<strong>Nom :</strong>
+{{ $demande->employe->nom ?? '-' }}
 {{ $demande->employe->prenom ?? '-' }}
 
 <br>
 
-{{-- Bureau : {{ $demande->bureau->libelle_bureau ?? '-' }}
-
-<br> --}}
-
-Date demande :
-
+<strong>Date demande :</strong>
 {{ \Carbon\Carbon::parse($demande->date_demande)->format('d/m/Y') }}
 
 </td>
 
 <td width="50%">
 
-<strong>Traiteur</strong><br>
+<strong>Informations du Traiteur</strong>
 
-Nom : {{ $authUser->name }}
+<br><br>
+
+<strong>Nom :</strong>
+{{ $authUser->name }}
 
 <br>
 
-Statut :
-
+<strong>Statut :</strong>
 {{ $demande->status }}
 
 </td>
@@ -88,19 +135,20 @@ Statut :
 
 <br>
 
+<!-- TABLEAU -->
 <table>
 
 <thead>
 
 <tr>
 
-<th>Code</th>
+<th width="15%">Code</th>
 
-<th>Immobilisation</th>
+<th width="30%">Immobilisation</th>
 
-<th>Description</th>
+<th width="40%">Description</th>
 
-<th>Statut</th>
+<th width="15%">Statut</th>
 
 </tr>
 
@@ -124,25 +172,26 @@ Statut :
 
 </table>
 
-<br><br>
+<br><br><br>
 
-<table>
+<!-- SIGNATURES -->
+<table class="signatures">
 
 <tr>
 
-<td>
+<td width="50%" style="text-align:center">
 
-Signature demandeur
+<strong>Signature du Demandeur</strong>
 
-<br><br><br>
+<br><br><br><br>
 
 </td>
 
-<td>
+<td width="50%" style="text-align:center">
 
-Signature ordonnateur
+<strong>Signature de l'Ordonnateur</strong>
 
-<br><br><br>
+<br><br><br><br>
 
 </td>
 
